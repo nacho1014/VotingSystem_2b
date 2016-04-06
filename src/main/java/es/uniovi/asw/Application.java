fb535888bb08a99df3059e3bd90790a070592e66
Nauce <naucelg@hotmail.com>
2016-04-06 23:47:17 +0200

    Integración de JSF con SpringBoot + BootsFaces - ButterFaces - PrimeFaces

--- src/main/java/es/uniovi/asw/Application.java
+++ src/main/java/es/uniovi/asw/Application.java
@@ -1,17 +1,67 @@
 package es.uniovi.asw;
 
-import org.springframework.boot.Banner;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.faces.webapp.FacesServlet;
+
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@EnableAutoConfiguration
+import es.uniovi.asw.configuration.ViewScope;
+
 @SpringBootApplication
-public class Application {
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"es.uniovi.asw"})
+public class Application extends SpringBootServletInitializer {
 
-  public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(Application.class);
-    app.setBannerMode(Banner.Mode.OFF);
-    app.run(args);
-  }
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.run(args);
+    }
+
+    @Bean
+    public ServletRegistrationBean facesServletRegistraiton() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new FacesServlet(), "*.xhtml");
+        registration.setName("Faces Servlet");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+        return servletContext -> {
+            servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+            servletContext.setInitParameter("javax.faces.FACELETS_SKIP_COMMENTS", Boolean.TRUE.toString());
+        };
+    }
+
+    @Bean
+    public static CustomScopeConfigurer customScopeConfigurer(){
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        Map<String, Object> scopes = new HashMap<String, Object>();
+        scopes.put("view", new ViewScope());
+        configurer.setScopes(scopes);
+        return configurer;
+    }
+    
+    @Bean
+    public WebMvcConfigurerAdapter forwardToIndex() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName(
+                        "redirect:/index.xhtml");
+            }
+        };
+    }
 }
