85a241b286eae23fa93683667142d617fdafa6a8
jorge19rio <jorgeriopedrefisica12a@gmail.com>
2016-04-07 22:26:25 +0200

    FailsTest

---
+++ src/test/java/es/uniovi/asw/FailsTest.java
@@ -0,0 +1,14 @@
+package es.uniovi.asw;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FailsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
