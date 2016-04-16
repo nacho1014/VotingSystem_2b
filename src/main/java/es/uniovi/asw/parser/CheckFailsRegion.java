f3ee837d12986e83fa6ed156e62fa12e0b96bdb1
jorge19rio <jorgeriopedrefisica12a@gmail.com>
2016-04-16 14:02:20 +0200

    log de errores en Region

--- src/main/java/es/uniovi/asw/parser/CheckFailsRegion.java
+++ src/main/java/es/uniovi/asw/parser/CheckFailsRegion.java
@@ -3,12 +3,17 @@ package es.uniovi.asw.parser;
 import es.uniovi.asw.model.Constituency;
 import es.uniovi.asw.model.PollingPlace;
 import es.uniovi.asw.model.Region;
+import es.uniovi.asw.reportwriter.WReportR;
+import es.uniovi.asw.reportwriter.WriteReport;
 
 public class CheckFailsRegion {
 
+	private static final WriteReport reporter = new WReportR();
+	public static String file;
+	
 	public static boolean comprobarFallosNombre(Region r){
 		if (r.getName() == null || r.getName().equals("")) {
-			System.out.println(" Nombre vacío --- ---");
+			reporter.report(file + " Nombre vacío --- ---");
 			return false;
 		}
 		return true;
@@ -16,7 +21,7 @@ public class CheckFailsRegion {
 	
 	public static boolean comprobarFallosConstituency(Constituency c){
 		if (c.getName() == null || c.getName().equals("")) {
-			System.out.println(" Nombre vacío --- ---");
+			reporter.report(file + " Nombre vacío --- ---");
 			return false;
 		}
 		return true;
@@ -24,10 +29,10 @@ public class CheckFailsRegion {
 	
 	public static boolean comprobarFallosPollingPlace(PollingPlace p){
 		if (p.getId() == null)  {
-			System.out.println(" PollingPlace vacío --- ---");
+			reporter.report(file + " PollingPlace vacío --- ---");
 			return false;
 		} else if (p.getId()<0) {
-			System.out.println(" PollingPlace no válido --- " + p.getId() + " ---");
+			reporter.report(file + " PollingPlace no válido --- " + p.getId() + " ---");
 			return false;
 		}
 		return true;
