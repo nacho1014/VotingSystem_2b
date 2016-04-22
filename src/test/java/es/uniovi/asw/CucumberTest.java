acca65cfd840ca4455a83547137ab5b0e3ba6626
Nauce <naucelg@hotmail.com>
2016-04-22 05:09:36 +0200

    Fin travis

--- src/test/java/es/uniovi/asw/CucumberTest.java
+++ src/test/java/es/uniovi/asw/CucumberTest.java
@@ -2,14 +2,11 @@ package es.uniovi.asw;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import es.uniovi.asw.dbupdate.RepositoryConfiguration;
 
 @RunWith(Cucumber.class)
-@SpringApplicationConfiguration(classes = {Application.class, RepositoryConfiguration.class})
 @IntegrationTest("server.port:8080")
 @CucumberOptions(features = "src/test/resources/features")
 public class CucumberTest{
